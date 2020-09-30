c3d9f5b582ae7b23df94aabe146069f5a3b82d8d
Raphaguizan <raphaelguizan@gmail.com>
2020-09-29 22:03:47 -0300

    segundo trabalho de compiladores

--- analise-lexica/logproc.py
+++ analise-lexica/logproc.py
@@ -12,15 +12,21 @@ tokens = [
 
 def t_TIMESTAMP(t):
     # Regular expression for TIMESTAMP
+    r'[0-2][0-9]:[0-5][0-9]:[0-5][0-9].[0-9]{1,6}\ [-+][0-9]{4}(?=\t)'
+    t.type = 'TIMESTAMP'
     return t
 
 def t_PROC(t):
     # Regular expression for PROC
+    r'\t[A-Za-z\.-]*\t'
+    t.type = 'PROC'
     t.value = t.value[1:len(t.value) - 1]
     return t
 
 def t_MESSAGE(t):
     # Regular expression for MESSAGE
+    r'[^\t]*\n'
+    t.type = 'MESSAGE'
     t.value = t.value[:len(t.value) - 1]
     return t
 
@@ -42,6 +48,16 @@ class LogProcLexer:
 
     def collect_messages(self):
         # Do your magic here
+        tokens = []
+        while True:
+            token = self.lexer.token()
+            if not token:
+                break 
+            if token.type == 'PROC' and token.value == "kernel":
+                token = self.lexer.token()
+                if token.type == 'MESSAGE':
+                    tokens.append(token)
+        return tokens
         
 if __name__ == '__main__':
     print(LogProcLexer().collect_messages())
